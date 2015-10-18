3d43bf185b79e3f01a5b13bf7b9347bc264c3486
Ivan Ramos Pagnossin <ivan.pagnossin@gmail.com>
2015-10-18 02:02:47 -0200

    My implementation of requested assignment. It is missing overall documentation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,48 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions below enhances dealing with the inverse of large matrices:
+## it wraps a given square matrix and its inverse, lazily evaluated, in
+## a structure composed of two getters and two setters:
+## Example:
+##
+## x <- matrix(rnorm(100), 10, 10)
+## structured_x <- makeCacheMatrix(x)
+## structured_x$getinv()
 
-## Write a short comment describing this function
-
+## It wrapps a given matrix x in a structure
+## which caches its inverse.
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    # TODO: check if x is square
+    
+    inv_x <- NULL
+    
+    # Getters
+    get <- function() x
+    getinv <- function() inv_x
+    
+    # Setters
+    set <- function(new_x) {
+        x <<- new_x
+        inv_x <<- NULL
+    }
+    setinv <- function(inv) inv_x <<- inv
+    
+    # Exposed elements of this structure
+    list(set = set, get = get,
+         setinv = setinv, getinv = getinv)
 }
 
-
-## Write a short comment describing this function
-
+## Return the inverse of structured matrix x,
+## either cached or determined on demand.
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    # It retrieves cached inverse
+    inv_x <- x$getinv()
+    
+    # If no cached inverse exists, calculates it from x and cache it
+    if(is.null(inv_x)) {
+        inv_x <- solve(x$get())
+        x$setinv(inv_x)
+    }
+    
+    inv_x
 }
